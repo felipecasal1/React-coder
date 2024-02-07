e7346f13f5f6f0de05f88ef42169bdef9f8bf9ca
felipecasal1 <felipecasalcornejo@gmail.com>
2024-02-07 20:52:52 -0300

    correcion de problemas segunda pre entrega

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import DarkBackround from './components/DarkBackround/DarkBackround';
+
 
 function App() {
   return (
